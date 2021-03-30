6375c0090e1cba047b3fe9fa5f2cd09a2e3af451
Matthew Mirvish <matthew@mm12.xyz>
2021-03-30 11:23:31 -0400

    elanspi: add more gdata

--- libfprint/drivers/elanspi.c
+++ libfprint/drivers/elanspi.c
@@ -36,16 +36,33 @@ struct _FpiDeviceElanSpi {
 	FpImageDevice parent;
 
 	/* sensor info */
-	unsigned char sensor_width, sensor_height, sensor_ic_version, sensor_id;
+	guint8 sensor_width, sensor_height, sensor_ic_version, sensor_id;
 	gboolean sensor_otp;
+	guint8 sensor_vcm_mode;
 	/* end sensor info */
 
 	/* init info */
-	unsigned char sensor_raw_version, sensor_init_reg17;
+	guint8 sensor_raw_version, sensor_reg_17;
+	guint8 sensor_reg_vref1, sensor_reg_28, sensor_reg_27, sensor_reg_dac2;
 	/* end init info */
+
+	union {
+		struct {
+			guint8 dac_value;
+			guint8 otp_timeout;
+			guint8 line_ptr;
+		} old_data;
+		struct {
+			guint16 gdac_value;
+		} hv_data;
+	};
+
+	/* generic temp info for async reading */
+	guint8 sensor_status;
 
 	/* background / calibration parameters */
 	guint16 *bg_image;
+	guint16 *last_image;
 
 	/* active SPI status info */
 	int spi_fd;
@@ -168,3 +185,66 @@ enum elanspi_write_regtable_state {
 	ELANSPI_WRTABLE_ITERATE,
 	ELANSPI_WRTABLE_NSTATES
 };
+
+static void dev_open(FpImageDevice *dev) {
+	FpiDeviceElanSpi *self = FPI_DEVICE_ELANSPI(dev);
+	GError *err = NULL;
+
+	G_DEBUG_HERE();
+
+	int spi_fd = open(fpi_device_get_udev_data(FP_DEVICE(dev), FP_DEVICE_UDEV_SUBTYPE_SPIDEV), O_RDWR);
+	if (spi_fd < 0) {
+		g_set_error(&err, G_IO_ERROR, g_io_error_from_errno(errno), "unable to open spi");
+		fpi_image_device_open_complete(dev, err);
+		return;
+	}
+
+	self->spi_fd = spi_fd;
+
+	fpi_image_device_open_complete(dev, NULL);
+}
+
+static void dev_close(FpImageDevice *dev) {
+	FpiDeviceElanSpi *self = FPI_DEVICE_ELANSPI(dev);
+
+	if (self->spi_fd >= 0) {
+		self->spi_fd = -1;
+		close(self->spi_fd);
+	}
+	fpi_image_device_close_complete(dev, NULL);
+}
+
+static void fpi_device_elanspi_init(FpiDeviceElanSpi *self) {
+	self->spi_fd = -1;
+	self->sensor_id = 0xff;
+	self->bg_image = NULL;
+}
+
+static void fpi_device_elanspi_finalize(GObject *this) {
+	FpiDeviceElanSpi *self = FPI_DEVICE_ELANSPI(this);
+
+	g_clear_pointer(&self->bg_image, g_free);
+
+	G_OBJECT_CLASS(fpi_device_elanspi_parent_class)->finalize(this);
+}
+
+static void fpi_device_elanspi_class_init(FpiDeviceElanSpiClass *klass) {
+	FpDeviceClass *dev_class = FP_DEVICE_CLASS (klass);
+	FpImageDeviceClass *img_class = FP_IMAGE_DEVICE_CLASS (klass);
+
+	dev_class->id = "elanspi";
+	dev_class->full_name = "ElanTech Embedded Fingerprint Sensor";
+	dev_class->type = FP_DEVICE_TYPE_UDEV;
+	dev_class->id_table = elanspi_id_table;
+	dev_class->scan_type = FP_SCAN_TYPE_SWIPE;
+	dev_class->nr_enroll_stages = 7; // these sensors are very hit or miss, may as well record a few extras
+
+	img_class->bz3_threshold = 10;
+	img_class->img_open = dev_open;
+	img_class->activate = dev_activate;
+	img_class->deactivate = dev_deactivate;
+	img_class->change_state = dev_change_state;
+	img_class->img_close = dev_close;
+
+	G_OBJECT_CLASS(klass)->finalize = fpi_device_elanspi_finalize;
+}
